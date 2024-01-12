222877db4aa49ad62c87dd9979224e4ed4e35334
Diego Pagini <dhpagini@knowmadmood.com>
2024-01-12 08:41:42 +0100

    counter

--- lib/presentation/screens/cubit_counter_screen.dart
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -5,6 +5,11 @@ class CubitCounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cubit Counter'),
+      ),
+      body: const Center(child: Text('Counter value: xxx')),
+    );
   }
 }
