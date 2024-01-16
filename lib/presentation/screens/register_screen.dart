da8884947399c560eefe576db18479b051a70fa2
Diego Pagini <dhpagini@knowmadmood.com>
2024-01-16 16:59:57 +0100

    regular form

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -43,39 +43,81 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  // To use regular forms without a state manager.
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String email = '';
+  String password = '';
+  String userName = '';
 
   @override
   Widget build(BuildContext context) {
     return Form(
+        key: _formKey,
         child: Column(
-      children: [
-        const CustomTextFormField(
-          label: 'Nombre de usuario',
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        const CustomTextFormField(
-          label: 'Correo electronico',
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        const CustomTextFormField(
-          label: 'Contraseña',
-          obscureText: true,
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        FilledButton.tonalIcon(
-          onPressed: () {},
-          icon: const Icon(Icons.save),
-          label: const Text('Crear usuario'),
-        ),
-      ],
-    ));
+          children: [
+            CustomTextFormField(
+              label: 'Nombre de usuario',
+              onChanged: (value) => userName = value,
+              validator: (value) {
+                if (value == null || value.isEmpty) return 'Campo requerido';
+                if (value.trim().isEmpty) return 'Campo requerido';
+                if (value.length < 6) return 'Más de 6 letras';
+                return null;
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomTextFormField(
+                label: 'Correo electronico',
+                onChanged: (value) => email = value,
+                validator: (value) {
+                  if (value == null || value.isEmpty) return 'Campo requerido';
+                  if (value.trim().isEmpty) return 'Campo requerido';
+                  final emailRegExp = RegExp(
+                    r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+                  );
+                  if (!emailRegExp.hasMatch(value)) return 'Correo no válido';
+
+                  return null;
+                }),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomTextFormField(
+                label: 'Contraseña',
+                obscureText: true,
+                onChanged: (value) => password = value,
+                validator: (value) {
+                  if (value == null || value.isEmpty) return 'Campo requerido';
+                  if (value.trim().isEmpty) return 'Campo requerido';
+                  if (value.length < 6) return 'Más de 6 letras';
+
+                  return null;
+                }),
+            const SizedBox(
+              height: 20,
+            ),
+            FilledButton.tonalIcon(
+              onPressed: () {
+                final isValid = _formKey.currentState!
+                    .validate(); // To activate validations.
+                if (!isValid) return;
+
+                print('$userName, $email, $password');
+              },
+              icon: const Icon(Icons.save),
+              label: const Text('Crear usuario'),
+            ),
+          ],
+        ));
   }
 }
