ff8d0b2410dc28d26da07087e8146d7c6c612bfe
Diego Pagini <dhpagini@knowmadmood.com>
2024-01-17 09:18:09 +0100

    formz

---
+++ lib/infrastructure/input/username.dart
@@ -0,0 +1,22 @@
+import 'package:formz/formz.dart';
+
+// Define input validation errors
+enum UsernameError { empty, length }
+
+// Extend FormzInput and provide the input type and error type.
+class Username extends FormzInput<String, UsernameError> {
+  // Call super.pure to represent an unmodified form input.
+  const Username.pure() : super.pure('');
+
+  // Call super.dirty to represent a modified form input.
+  const Username.dirty(String value) : super.dirty(value);
+
+  // Override validator to handle validating a given input value.
+  @override
+  UsernameError? validator(String value) {
+    if (value.isEmpty || value.trim().isEmpty) return UsernameError.empty;
+    if (value.length < 6) return UsernameError.length;
+
+    return null;
+  }
+}
